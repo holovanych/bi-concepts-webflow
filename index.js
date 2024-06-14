0c91eca844fead728f59f37a77e3a996a78b0632
holovanych <t.holovanych@gmail.com>
2024-06-14 19:53:13 +0300

    header desktp menu

--- index.js
+++ index.js
@@ -1,58 +1,117 @@
+/* === Header Events === */
+
+const desktopHeader = document.querySelector(".main-header");
+let lastScrollTop = 0;
+window.addEventListener("scroll", () => {
+  const st = document.documentElement.scrollTop;
+  if (st > 200) {
+    desktopHeader.classList.add("header-scrolled");
+  } else {
+    desktopHeader.classList.remove("header-scrolled");
+  }
+  if (st > lastScrollTop && st > 500) {
+    desktopHeader.classList.add("hide");
+  } else {
+    desktopHeader.classList.remove("hide");
+  }
+  lastScrollTop = st <= 0 ? 0 : st;
+});
+
+window.scroll();
+
+const dropdownMenuActiveLinks = [
+  ...document.querySelectorAll(".rl_navbar1_dropdown-link.w--current"),
+];
+if (dropdownMenuActiveLinks) {
+  dropdownMenuActiveLinks.forEach((item) => {
+    const dropDown = item.closest(".rl_navbar1_menu-dropdown");
+    const dropdownParentLink = dropDown.querySelector(".rl_navbar1_link-text");
+    setTimeout(function () {
+      dropdownParentLink.classList.add("w--current");
+    }, 100);
+  });
+}
+
 /* === Home Page - Competencies Section === */
 
 const competenciesLinks = document.querySelectorAll(
   ".competencies-links__link"
 );
-competenciesLinks.forEach((link) => {
-  const anchor = link.dataset.anchor;
-  const href = link.getAttribute("href") + "#" + anchor;
-  link.setAttribute("href", href);
-});
 
-const competenciesLinkTitles = document.querySelectorAll(
-  ".competencies-links__title"
-);
-competenciesLinkTitles.forEach((title) => {
-  const listItem = title.closest(".competencies-links__item");
-  const hiddenTitle = listItem.querySelector(
-    ".competencies-links__hidden-title-with-line-break"
+if (competenciesLinks) {
+  competenciesLinks.forEach((link) => {
+    const anchor = link.dataset.anchor;
+    const href = link.getAttribute("href") + "#" + anchor;
+    link.setAttribute("href", href);
+  });
+
+  const competenciesLinkTitles = document.querySelectorAll(
+    ".competencies-links__title"
   );
-  title.innerHTML = removeTagsExceptBr(hiddenTitle.innerHTML);
-});
+  competenciesLinkTitles.forEach((title) => {
+    const listItem = title.closest(".competencies-links__item");
+    const hiddenTitle = listItem.querySelector(
+      ".competencies-links__hidden-title-with-line-break"
+    );
+    title.innerHTML = removeTagsExceptBr(hiddenTitle.innerHTML);
+  });
+}
 
 /* === Home Page - Testimonials Slider === */
 
-const testimonialSwiper = new Swiper(".swiper.testimonials-slider", {
-  loop: true,
-  /*autoplay: {
-    delay: 5000,
-  },*/
-  breakpoints: {
-    320: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    980: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-  },
+const testimonialSlider = document.querySelector(".swiper.testimonials-slider");
 
-  navigation: {
-    prevEl: ".testimonials-slider-arrows .navigation-arrow-left",
-    nextEl: ".testimonials-slider-arrows .navigation-arrow-right",
-  },
+if (testimonialSlider) {
+  const testimonialSwiper = new Swiper(testimonialSlider, {
+    loop: true,
+    /*autoplay: {
+      delay: 5000,
+    },*/
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      980: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
 
-  pagination: {
-    el: ".testimonials-slider-dots",
-    clickable: true,
-  },
+    navigation: {
+      prevEl: ".testimonials-slider-arrows .navigation-arrow-left",
+      nextEl: ".testimonials-slider-arrows .navigation-arrow-right",
+    },
 
-});
+    pagination: {
+      el: ".testimonials-slider-dots",
+      clickable: true,
+    },
+  });
+}
+
+/* Footer */
+
+const footerSubMenuActiveLinks = [
+  ...document.querySelectorAll(
+    ".footer__menu-child-wrapper .footer__child-link.w--current"
+  ),
+];
+if (footerSubMenuActiveLinks) {
+  footerSubMenuActiveLinks.forEach((item) => {
+    const subMenu = item.closest(".footer__sub-menu-wrapper");
+    const subMenuMainLink = subMenu.querySelector(
+      ".footer__link.sub-menu-main-link"
+    );
+    setTimeout(function () {
+      subMenuMainLink.classList.add("w--current");
+    }, 100);
+  });
+}
 
 /* === COMMON JS FUNCTIONS === */
 
